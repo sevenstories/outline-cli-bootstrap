0c98340cbc5f2951c6056070ea2ef78e63839e97
Joe Wheatley <joew@fnvi.co.uk>
2019-02-21 12:28:11 +0000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fnvi-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'outline';
+}
